607fd062f4d6388ab14da2e8ab65084bc3317eb8
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-06-03 22:30:10 +0300

    WIP: Table mock (header).

--- src/App.js
+++ src/App.js
@@ -5,7 +5,86 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-          Budget SPA (ReactJS)
+          <table>
+            <thead>
+
+              {/* Months */}
+              <tr>
+                <th></th>
+                <th colSpan="2">January</th>
+                <th colSpan="2">February</th>
+                <th colSpan="2">March</th>
+                <th colSpan="2">April</th>
+                <th colSpan="2">May</th>
+                <th colSpan="2">June</th>
+                <th colSpan="2">July</th>
+                <th colSpan="2">August</th>
+                <th colSpan="2">September</th>
+                <th colSpan="2">October</th>
+                <th colSpan="2">November</th>
+                <th colSpan="2">December</th>
+              </tr>
+
+              {/* Plan/Fact */}
+              <tr>
+                <th></th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+                <th>Plan</th>
+                <th>Fact</th>
+              </tr>
+
+              {/* Total */}
+              <tr>
+                <th></th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+                <th>0</th>
+              </tr>
+    
+            </thead>
+          </table>
         </div>
     );
   }
