985ff865e0a7d6b03ac963abd9ba4a9d283d448c
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-06-10 14:35:51 +0300

    Just another test row.

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,7 @@ const initialState = {
   categories: [
     { id: 10, order: 10, type: 'income', name: 'Salary' },
     { id: 20, order: 20, type: 'income', name: 'Bank Deposit' },
+    { id: 25, order: 25, type: 'income', name: 'Business' },
     { id: 30, order: 30, type: 'outgo', name: 'Food' },
     { id: 40, order: 40, type: 'outgo', name: 'Car' },
     { id: 50, order: 50, type: 'outgo', name: 'Dogs' }
@@ -10,6 +11,7 @@ const initialState = {
   operations: [
     { id: 10, categoryId: 10, month: 6, plan: 10, fact: 20 },
     { id: 15, categoryId: 20, month: 6, plan: 33, fact: 3 },
+    { id: 16, categoryId: 25, month: 6, plan: 87, fact: 77 },
     { id: 20, categoryId: 20, month: 7, plan: 100, fact: 0 },
     { id: 30, categoryId: 50, month: 6, plan: 600, fact: 900 },
   ]
