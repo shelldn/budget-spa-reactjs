ec2c54f1e5dcc7196d011d71087afcbe46455631
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-06-11 00:07:04 +0300

    One way of doing totals.

--- src/BudgetTable/Total.js
+++ src/BudgetTable/Total.js
@@ -1,35 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Total = () => (
+let Total = ({ totals }) => (
 
   <tr>
     <th></th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
+    {[...totals].map((t, idx) => 
+      <th key={idx}>{t}</th>
+    )}
   </tr>
 
 );
+
+function* mapMonthsToTotals(months, categories, operations) {
+  for (var i = 0; i < months.length; i++) {
+    let op = operations.filter(o => o.month === months[i]).reduce((a, b) => ({ 
+      plan: a.plan + (categories.some(c => c.type === 'income' && c.id === b.categoryId) ? 1 : -1) * b.plan,
+      fact: a.fact + (categories.some(c => c.type === 'income' && c.id === b.categoryId) ? 1 : -1) * b.fact
+    }), { plan: 0, fact: 0 });
+    yield op.plan;
+    yield op.fact;
+  }
+}
+
+const mapStateToProps = ({ months, categories, operations }) => ({
+  totals: mapMonthsToTotals(months, categories, operations)
+});
+
+Total = connect(
+  mapStateToProps
+)(Total);
 
 export default Total;
