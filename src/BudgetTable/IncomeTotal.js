048a8c727d9dcdb0c720a9de15e594b7d52fcb15
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-06-10 14:26:48 +0300

    IncomeTotals working.

--- src/BudgetTable/IncomeTotal.js
+++ src/BudgetTable/IncomeTotal.js
@@ -1,35 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const IncomeTotal = () => (
+let IncomeTotal = ({ totals }) => (
 
   <tr>
     <th></th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
-    <th>0</th>
+    {[...totals].map((t, idx) => 
+      <td key={idx}>{t}</td>
+    )}
   </tr>
 
 );
+
+function* mapMonthsToTotals(months, operations) {
+  for (var i = 0; i < months.length; i++) {
+    let op = operations
+      .filter(o => o.month === months[i])
+      .reduce((a, b) => ({
+        plan: a.plan + b.plan,
+        fact: a.fact + b.fact
+      }), { plan: 0, fact: 0 })
+    yield op.plan;
+    yield op.fact;
+  }
+}
+
+const mapStateToProps = ({ months, categories, operations }) => ({
+  totals: mapMonthsToTotals(months, operations.filter(o => categories.filter(c => c.type === 'income').some(c => c.id === o.categoryId)))
+});
+
+IncomeTotal = connect(
+  mapStateToProps
+)(IncomeTotal);
 
 export default IncomeTotal;
--- src/reducer.js
+++ src/reducer.js
@@ -9,8 +9,9 @@ const initialState = {
   ],
   operations: [
     { id: 10, categoryId: 10, month: 6, plan: 10, fact: 20 },
+    { id: 15, categoryId: 20, month: 6, plan: 33, fact: 3 },
     { id: 20, categoryId: 20, month: 7, plan: 100, fact: 0 },
-    { id: 30, categoryId: 50, month: 6, plan: 600, fact: 900 }
+    { id: 30, categoryId: 50, month: 6, plan: 600, fact: 900 },
   ]
 }
 
