a19f9d6e98643cc17ecde1f91ce1b762977662e1
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-06-27 20:20:50 +0300

    Table, Head refactoring.

--- src/BudgetTable/Head.js
+++ src/BudgetTable/Head.js
@@ -1,53 +1,28 @@
 import React from 'react';
-import TotalPlan from './TotalPlan';
-import TotalFact from './TotalFact';
+import { Body, Row, Cell } from '../Table';
 import { connect } from 'react-redux';
 
-let Head = ({ months, operations }) => (
+let Head = ({ months }) => (
 
-  <thead>
-
-    {/* Months */}
-    <tr>
-      <th></th>
-      {months.map(m => <th key={m} colSpan="2">{m}</th>)}
-    </tr>
-
-    {/* Plan/Fact */}
-    <tr>
-      <th></th>
-      {operations.map((o, idx) => <th key={idx}>{o.plan ? 'Plan' : 'Fact'}</th>)}
-    </tr>
-
-    {/* Totals */}
-    <tr>
-      <th></th>
-      {operations.map((o, idx) => o.plan
-        ? <TotalPlan key={idx} month={o.month} />
-        : <TotalFact key={idx} month={o.month} />
+  <Body>
+    <Row>
+      <Cell />
+      {months.map(m => 
+        <Cell colSpan={2} key={m}>{m}</Cell>
       )}
-    </tr>
-
-  </thead>
-
-);
-
-function* mapMonthsToOperations(months) {
-  for (var i = 0; i < months.length; i++) {
-    yield {
-      plan: true,
-      month: months[i]
-    };
-    yield {
-      plan: false,
-      month: months[i]
-    };
-  }  
-}
+    </Row>
+    <Row>
+      <Cell />
+      {months.map(_ => [
+        <Cell>Plan</Cell>,
+        <Cell>Fact</Cell>
+      ])}
+    </Row>
+  </Body>
+)
 
 const mapStateToProps = (state) => ({
-  months: state.months,
-  operations: [...mapMonthsToOperations(state.months)]
+  months: state.months
 });
 
 Head = connect(
--- src/BudgetTable/index.js
+++ src/BudgetTable/index.js
@@ -1,39 +1,16 @@
 import React from 'react';
-import Table, { Body, Row, Cell } from '../Table';
+import Table from '../Table';
+import Head from './Head';
 import IncomeBody from './IncomeBody';
 import OutgoBody from './OutgoBody';
-import { connect } from 'react-redux';
 import './index.css';
 
-let BudgetTable = ({ months, categories }) => (
+const BudgetTable = () => (
   <Table>
-    <Body>
-      <Row>
-        <Cell />
-        {months.map(m => 
-          <Cell colSpan={2} key={m}>{m}</Cell>
-        )}
-      </Row>
-      <Row>
-        <Cell />
-        {months.map(_ => [
-          <Cell>Plan</Cell>,
-          <Cell>Fact</Cell>
-        ])}
-      </Row>
-    </Body>
+    <Head />
     <IncomeBody />
     <OutgoBody />
   </Table>
 );
-
-const mapStateToProps = (state) => ({
-  months: state.months,
-  categories: state.categories.list
-});
-
-BudgetTable = connect(
-  mapStateToProps
-)(BudgetTable);
 
 export default BudgetTable;
