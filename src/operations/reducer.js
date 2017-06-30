93e6c18a90071e7bd74f7db158ea6269e55e8c40
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-06-30 15:47:33 +0300

    Commit operation f-ty.

--- src/operations/reducer.js
+++ src/operations/reducer.js
@@ -1,6 +1,7 @@
 const FETCH = 'budget-io/operations/FETCH';
 const EDIT = 'budget-io/operations/EDIT';
 const CHANGE = 'budget-io/operations/CHANGE';
+const COMMIT = 'budget-io/operations/COMMIT';
 
 export const editId = (state = {}, action) => {
   switch (action.type) {
@@ -18,17 +19,39 @@ export const editId = (state = {}, action) => {
         id: action.payload.id,
         type: action.payload.type,
         value: action.payload.value
-      }
+      };
+
+    case COMMIT:
+      return {};
     
     default:
       return state;
   }
 }
 
+const operation = (state, action) => {
+  switch (action.type) {
+    case COMMIT:
+      if (action.payload.id !== state.id)
+        return state;
+
+      return {
+        ...state,
+        [action.payload.type]: action.payload.value
+      };
+    
+    default:
+      return state;
+  }
+};
+
 const operations = (state = [], action) => {
   switch (action.type) {
     case FETCH:
       return action.payload;    
+
+    case COMMIT:
+      return state.map(o => operation(o, action));
 
     default:
       return state;      
