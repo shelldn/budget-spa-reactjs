765e155db509b097aad3622c76041877f7fd2fb9
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-07-09 13:36:08 +0300

    Obsolete components removed.

--- src/BudgetTable/TotalFact.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { total } from './Total';
-
-const Total = ({ value }) => (
-  <th className="budget__total--fact">{value}</th>
-)
-
-export default total(Total, 'fact');
