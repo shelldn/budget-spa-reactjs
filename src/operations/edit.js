8c7678b5e6c5581a8ffc86cdbb61309d8b43fb56
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-12-31 01:51:47 +0200

    operations/{Plan,Fact} components removed as redundant.

--- src/operations/edit.js
+++ src/operations/edit.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import Plan from './Plan';
-import Fact from './Fact';
+import { EditPlanModel } from './index.js';
+import EditPlan from './EditPlan';
+import DisplayPlan from './DisplayPlan';
+import { EditFactModel } from './index.js';
+import EditFact from './EditFact';
+import DisplayFact from './DisplayFact';
 
 const edit = (o) => [
-  <Plan {...o} />,
-  <Fact {...o} />
+  o.plan instanceof EditPlanModel
+    ? <EditPlan {...o} />
+    : <DisplayPlan value={o.plan} />,
+
+  o.fact instanceof EditFactModel
+    ? <EditFact {...o} />
+    : <DisplayFact value={o.fact} />
 ];
 
 export default edit;
