2b21901350ab59731615c1e147b115787850d881
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-12-31 00:35:50 +0200

    Obsolete Operation component/reducer removed.

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,9 @@
 import { reducer as categories } from './categories';
 import { reducer as operations } from './operations';
-import operationEdit from './Operation/Edit.reducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
   months: (state = []) => state,
   categories,
-  operations,
-  operationEdit
+  operations
 });
