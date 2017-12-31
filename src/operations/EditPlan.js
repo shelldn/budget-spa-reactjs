6e4859b53d93eda145b041dd64dd9651064b3a56
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-12-31 20:10:27 +0200

    EditPlan adjusted to auto-select contents.

--- src/operations/EditPlan.js
+++ src/operations/EditPlan.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const EditPlan = ({ plan }) => (
-  <td>
-    <input
-      autoFocus
-      type="text"
-      defaultValue={plan.value}
-    />
-  </td>
-);
+class EditPlan extends Component {
+
+  componentDidMount() {
+    this.input.select();
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <td>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            ref={i => this.input = i}
+            autoFocus
+            type="text"
+            defaultValue={this.props.plan.value}
+          />
+        </form>
+      </td>
+    );
+  }
+}
 
 export default EditPlan;
